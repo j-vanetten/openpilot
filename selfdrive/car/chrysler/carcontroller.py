359ecf484265d220751898a396a413e2fde72421
John <4vanetten@gmail.com>
2021-11-01 21:29:13 -0600

    small fixes

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -147,14 +147,14 @@ class CarController():
       if self.last_brake is None:
         self.last_brake = min(0., brake_target - 0.2)
       else:
-        lBrake = math.floor(self.last_brake * 100) / 100
-        tBrake = math.floor(brake_target * 100) / 100
+        # lBrake = math.floor(self.last_brake * 100) / 100
+        # tBrake = math.floor(brake_target * 100) / 100
         # if tBrake < lBrake:
         #   self.last_brake = max(self.last_brake - 0.02, tBrake)
         # elif tBrake > lBrake:
         #   self.last_brake = min(self.last_brake + 0.02, tBrake)
 
-        self.last_brake = (lBrake + tBrake) / 2
+        self.last_brake = (self.last_brake + brake_target) / 2
       print(f"last_brake={self.last_brake}, brake_target={brake_target}")
     else:
       self.last_brake = None
