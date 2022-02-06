b14a87a8c7c9bc40389827ce95921ce36e47b4e4
John <4vanetten@gmail.com>
2022-02-06 11:40:52 -0700

    controls

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -80,17 +80,17 @@ static int chrysler_rx_hook(CANPacket_t *to_push) {
 
     // enter controls on rising edge of ACC, exit controls on ACC off
     else if (addr == 500) {
-//      int cruise_available = ((GET_BYTE(to_push, 2) & 0x18U) >> 3) == 3U;
-//      if (cruise_available) {
-//        int cruise_engaged = ((GET_BYTE(to_push, 2) & 0x38U) >> 3) == 7U;
-//        if (cruise_engaged) {
-//          controls_allowed = 1;
-//        }
-//        // keep control if stopped when cruise disengaged
-//        else if (!cruise_engaged && (vehicle_speed > CHRYSLER_GAS_THRSLD || (cruise_engaged_prev && vehicle_moving))) {
-//          controls_allowed = 0;
-//        }
-//      }
+      int cruise_available = ((GET_BYTE(to_push, 2) & 0x18U) >> 3) == 3U;
+      if (cruise_available) {
+        int cruise_engaged = ((GET_BYTE(to_push, 2) & 0x38U) >> 3) == 7U;
+        if (cruise_engaged) {
+          controls_allowed = 1;
+        }
+        // keep control if stopped when cruise disengaged
+        else if (!cruise_engaged && (vehicle_speed > CHRYSLER_GAS_THRSLD || (cruise_engaged_prev && vehicle_moving))) {
+          controls_allowed = 0;
+        }
+      }
     }
 
     // update speed
