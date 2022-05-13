1b14bc7487b6baa765a32a8e3fc1d28c88b46ba5
John <4vanetten@gmail.com>
2022-05-12 19:59:00 -0600

    latest xps radar code

--- selfdrive/car/chrysler/radar_interface.py
+++ selfdrive/car/chrysler/radar_interface.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import math
-from common.numpy_fast import clip
-from opendbc.can.parser import CANParser
+
 from cereal import car
+from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import RadarInterfaceBase
 from selfdrive.car.chrysler.values import DBC, CAR
 
-RADAR_MSGS_C = list(range(0x2c2, 0x2d4+2, 2))  # c_ messages 706,...,724
-RADAR_MSGS_D = list(range(0x2a2, 0x2b4+2, 2))  # d_ messages
+RADAR_MSGS_C = list(range(0x2c2, 0x2d4 + 2, 2))  # c_ messages 706,...,724
+RADAR_MSGS_D = list(range(0x2a2, 0x2b4 + 2, 2))  # d_ messages
 LAST_MSG = max(RADAR_MSGS_C + RADAR_MSGS_D)
-NUMBER_MSGS = len(RADAR_MSGS_C) + len(RADAR_MSGS_D)
+
 
 def _create_radar_can_parser(car_fingerprint):
-  msg_n = len(RADAR_MSGS_C)
+  msg_c_n = len(RADAR_MSGS_C)
+  msg_d_n = len(RADAR_MSGS_D)
   # list of [(signal name, message name or number), (...)]
   # [('RADAR_STATE', 1024),
   #  ('LONG_DIST', 1072),
@@ -20,18 +21,18 @@ def _create_radar_can_parser(car_fingerprint):
   #  ('LONG_DIST', 1074),
   #  ('LONG_DIST', 1075),
 
-  signals = list(zip(['LONG_DIST'] * msg_n +
-                ['LAT_ANGLE'] * msg_n +
-                ['REL_SPEED'] * msg_n +
-                ['PROBABILITY'] * msg_n +
-                ['MEASURED'] * msg_n,
-                RADAR_MSGS_C * 2 +  # LONG_DIST, LAT_DIST
-                RADAR_MSGS_D * 3))  # REL_SPEED, PROBABILITY, MEASURED
+  signals = list(zip(['LONG_DIST'] * msg_c_n +
+                     ['LAT_ANGLE'] * msg_c_n +
+                     ['REL_SPEED'] * msg_d_n +
+                     ['MEASURED'] * msg_d_n +
+                     ['PROBABILITY'] * msg_d_n,
+                     RADAR_MSGS_C * 2 +  # LONG_DIST, LAT_DIST
+                     RADAR_MSGS_D * 3))  # REL_SPEED, MEASURED
 
   checks = list(zip(RADAR_MSGS_C +
                     RADAR_MSGS_D,
-                    [20] * msg_n +  # 20Hz (0.05s)
-                    [20] * msg_n))  # 20Hz (0.05s)
+                    [20] * msg_c_n +  # 20Hz (0.05s)
+                    [20] * msg_d_n))  # 20Hz (0.05s)
 
   return CANParser(DBC[car_fingerprint]['radar'], signals, checks, 1)
 
@@ -49,7 +50,7 @@ class RadarInterface(RadarInterfaceBase):
     self.updated_messages = set()
     self.trigger_msg = LAST_MSG
 
-    self.yRel_multiplier = -1 if CP.carFingerprint in (CAR.PACIFICA_2017_HYBRID, CAR.PACIFICA_2018_HYBRID, CAR.PACIFICA_2019_HYBRID) else 1
+    self.yRel_multiplier = 1 if CP.carFingerprint in (CAR.JEEP_CHEROKEE, CAR.PACIFICA_2018, CAR.PACIFICA_2017_HYBRID, CAR.PACIFICA_2018_HYBRID) else -1
 
   def update(self, can_strings):
     vls = self.rcp.update_strings(can_strings)
@@ -74,16 +75,20 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[trackId].aRel = float('nan')
         self.pts[trackId].yvRel = float('nan')
 
+        # self.pts[trackId].yRel = float('nan')
+
       if 'LONG_DIST' in cpt:  # c_* message
         azimuth = (cpt['LAT_ANGLE'])
-        self.pts[trackId].dRel = math.cos(azimuth) * cpt['LONG_DIST']
-        self.pts[trackId].yRel = math.sin(azimuth) * cpt['LONG_DIST'] * self.yRel_multiplier
+        # self.pts[trackId].dRel = math.cos(azimuth) * cpt['LONG_DIST']
+        # self.pts[trackId].yRel = math.sin(azimuth) * cpt['LONG_DIST']
+        self.pts[trackId].dRel = cpt['LONG_DIST']
+        self.pts[trackId].yRel = math.tan(azimuth) * cpt['LONG_DIST'] * self.yRel_multiplier
       else:  # d_* message
         self.pts[trackId].vRel = cpt['REL_SPEED']
-        self.pts[trackId].measured = bool(cpt['MEASURED'])
+        self.pts[trackId].measured = bool(cpt['MEASURED']) and (cpt['PROBABILITY'] > 250)
 
     # We want a list, not a dictionary. Filter out LONG_DIST==0 because that means it's not valid.
-    ret.points = [x for x in self.pts.values() if x.measured and 250 > x.dRel > 0]
+    ret.points = [x for x in self.pts.values() if x.measured and (255 > x.dRel > 0)]
 
     self.updated_messages.clear()
     return ret
