edade0e77f91cda1aea141dfae45e5f3b34d1d48
John <4vanetten@gmail.com>
2022-11-12 21:16:20 -0700

    remove dup method

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -96,44 +96,6 @@ class VCruiseHelper:
 
     return max(self.v_cruise_kph, v_cruise_min)
 
-  def _update_v_cruise_non_pcm(self, CS, enabled, is_metric):
-    # handle button presses. TODO: this should be in state_control, but a decelCruise press
-    # would have the effect of both enabling and changing speed is checked after the state transition
-    if not enabled:
-      return
-
-    long_press = False
-    button_type = None
-
-    # should be CV.MPH_TO_KPH, but this causes rounding errors
-    v_cruise_delta = 1. if is_metric else 1.6
-
-    for b in CS.buttonEvents:
-      if b.type.raw in self.button_timers and not b.pressed:
-        if self.button_timers[b.type.raw] > CRUISE_LONG_PRESS:
-          return  # end long press
-        button_type = b.type.raw
-        break
-    else:
-      for k in self.button_timers.keys():
-        if self.button_timers[k] and self.button_timers[k] % CRUISE_LONG_PRESS == 0:
-          button_type = k
-          long_press = True
-          break
-
-    if button_type:
-      v_cruise_delta = v_cruise_delta * (5 if long_press else 1)
-      if long_press and self.v_cruise_kph % v_cruise_delta != 0:  # partial interval
-        self.v_cruise_kph = CRUISE_NEAREST_FUNC[button_type](self.v_cruise_kph / v_cruise_delta) * v_cruise_delta
-      else:
-        self.v_cruise_kph += v_cruise_delta * CRUISE_INTERVAL_SIGN[button_type]
-
-      # If set is pressed while overriding, clip cruise speed to minimum of vEgo
-      if CS.gasPressed and button_type in (ButtonType.decelCruise, ButtonType.setCruise):
-        self.v_cruise_kph = max(self.v_cruise_kph, CS.vEgo * CV.MS_TO_KPH)
-
-      self.v_cruise_kph = clip(round(self.v_cruise_kph, 1), V_CRUISE_MIN, V_CRUISE_MAX)
-
   def update_button_timers(self, CS):
     # increment timer for buttons still pressed
     for k in self.button_timers:
