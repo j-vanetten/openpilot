68382db210bcf1799fe646f1fe9f87f913c81463
John <4vanetten@gmail.com>
2022-12-30 11:11:56 -0700

    always allow

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -275,7 +275,7 @@ static int chrysler_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
     const SteeringLimits limits = (chrysler_platform == CHRYSLER_PACIFICA) ? CHRYSLER_STEERING_LIMITS :
                                   (chrysler_platform == CHRYSLER_RAM_DT) ? CHRYSLER_RAM_DT_STEERING_LIMITS : CHRYSLER_RAM_HD_STEERING_LIMITS;
     if (steer_torque_cmd_checks(desired_torque, -1, limits)) {
-      tx = 0;
+      //tx = 0;
     }
   }
 
