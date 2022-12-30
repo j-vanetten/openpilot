1f4fc697254eeb808c8ecc832bbd00e0b3dd1049
John <4vanetten@gmail.com>
2022-12-30 10:30:29 -0700

    derp

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -46,7 +46,7 @@ class CarState(CarStateBase):
     self.longControl = False
     self.longAvailable = False
     self.longEnabled = False
-    self.enginRpm = None
+    self.engineRpm = None
     self.engineTorque = None
     self.allowLong = True # CP.carFingerprint in (CAR.JEEP_CHEROKEE, CAR.JEEP_CHEROKEE_2019)
     self.torqMin = None
@@ -113,7 +113,7 @@ class CarState(CarStateBase):
       self.torqMin = cp.vl["ECM_TRQ"]["ENGINE_TORQ_MIN"]
       self.torqMax = cp.vl["ECM_TRQ"]["ENGINE_TORQ_MAX"]
       self.currentGear = cp.vl['TCM_A7']["CurrentGear"]
-      self.enginRpm = cp.vl["ECM_1"]["ENGINE_RPM"]
+      self.engineRpm = cp.vl["ECM_1"]["ENGINE_RPM"]
       self.engineTorque = cp.vl["ECM_1"]["ENGINE_TORQUE"]
       ret.jvePilotCarState.accFollowDistance = 0
     else:
